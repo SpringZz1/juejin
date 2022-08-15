a1b2fb94a9f90fdb565ce57011a227608dccc73b
SpringZz <wang90944@gmail.com>
2022-08-15 19:06:22 +0800

    修改样式

--- src/components/recommand/index.vue
+++ src/components/recommand/index.vue
@@ -11,12 +11,35 @@
           <a href="#">{{item.tags}}</a>
         </li>
       </ul>
+      <span class="option">
+        <i class="el-icon-close"></i>
+      </span>
       </div>
       <div class="content">
         <h3 class="title">test</h3>
-        <span class="word">garegerigjaeirgjaerigjaeiorg</span>
+        <div class="word">
+          文字过多文字过多文字过多文字过多文字过多文字过多文字过多文字过多文字过多文字过多文字过多文字过多文字过多文字过多文字过多
+          </div>
       </div>
-      <div class="image"></div>
+      <!-- 右侧图片 -->
+      <!-- 如果没有封面则不显示 -->
+      <div class="image" v-if="item.url !==''">
+        <img :src="item.url" alt="图片">
+      </div>
+      <div class="footer">
+        <div class="footer-item">
+          <i class="el-icon-view"></i>
+          <span>{{item.view}}</span>
+        </div>
+        <div class="footer-item">
+          <i class="el-icon-thumb"></i>
+          <span>{{item.likes}}</span>
+        </div>
+        <div class="footer-item">
+          <i class="el-icon-chat-round"></i>
+          <span>{{item.comments}}</span>
+        </div>
+      </div>
     </div>
   </div>
 </div>
@@ -32,12 +55,21 @@ export default {
         {
           author: '掘金酱',
           date: '16天前',
-          tags: '前端'
+          tags: '前端',
+          view: '1w',
+          likes: 2,
+          comments: '4',
+          // url: '@/assets/logo.png'
+          url: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png'
         },
         {
           author: '张员外',
           date: '一月前',
-          tags: 'Vue.js '
+          tags: 'Vue.js ',
+          view: '413',
+          likes: 413,
+          comments: '23',
+          url: ''
         }
       ]
     }
@@ -84,13 +116,21 @@ export default {
     content: '';
 }
 
+.option{
+  position: absolute;
+  top: 7px;
+  right: 20px;
+  color: #9090905a;
+  display: none;
+}
+
 .el-card__body{
   margin-top: -15px;
 }
 
 .main{
   position: relative;
-  height: 120px;
+  height: 130px;
   width: 100%;
   background-color: #fff;
 }
@@ -101,7 +141,7 @@ export default {
   content: '';
   width: 80%;
   /* height: 75px; */
-  top:110px;
+  top:125px;
   width: 95%;
   border-bottom: 1px solid #66666648;
 }
@@ -110,27 +150,76 @@ export default {
   background-color: #cccccc12;
 }
 
+.main:hover .option{
+  display: block;
+}
+
 .header{
   height: 20%;
 }
 
+.content{
+  position: relative;
+  width: 100%;
+  height: 80%;
+}
+
 .title{
   position: absolute;
-  left: 20px;
-  top: 35px;
+  left: 22px;
+  top: 13px;
   font-size: 16px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
 }
 
 .word{
   position: absolute;
-  left: 20px;
-  top:  60px;
+  left: 22px;
+  top: 47px;
   font-size: 13px;
   color: #909090;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  width: 95%;
 }
 
 .el-card__body{
   padding-left: 0;
   padding-right: 0;
+}
+
+.footer{
+  position: absolute;
+  left: 22px;
+  top: 100px;
+  text-decoration: none;
+}
+
+.footer-item{
+  display: inline-block;
+  margin-right: 20px;
+  color: #909090;
+}
+
+.footer-item:not(:first-child):hover{
+  color: #1171ee !important;
+}
+
+.footer span,a{
+  margin-left: 3px;
+  font-size: 13px;
+}
+
+.image img{
+  position: absolute;
+  width: 120px;
+  height: 80px;
+  top: 35px;
+  right: 15px;
+  border-radius: 2px;
+  object-fit: fill;
 }
 </style>
