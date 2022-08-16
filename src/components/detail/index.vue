f7e42628af03112b8110bbc47ee24a5aedf9330f
SpringZz <wang90944@gmail.com>
2022-08-16 19:10:25 +0800

    详情页面，添加接受参数方法

---
+++ src/components/detail/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    详情界面
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      id: 0
+    }
+  },
+  methods: {
+    getParamas () {
+      this.id = this.$route.params.id
+      console.log(this.id)
+    }
+  },
+  created () {
+    this.getParamas()
+  }
+}
+</script>
+
+<style>
+</style>
