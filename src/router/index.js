3a1d4698cc034ee3c2df043668d32f664837da1c
SpringZz <wang90944@gmail.com>
2022-08-16 19:09:26 +0800

    添加详情页路由

--- src/router/index.js
+++ src/router/index.js
@@ -39,6 +39,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/detail',
+    name: 'Detail',
+    component: () => import('../components/detail/index.vue')
   }
 ]
 
