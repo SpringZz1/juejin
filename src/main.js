44ba926c1bb7c201819e4cbb51a67a4ad7dde30e
SpringZz <wang90944@gmail.com>
2022-08-20 18:03:32 +0800

    注册axios

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import './plugins/axios'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-
+// eslint-disable-next-line no-unused-vars
+import axios from 'axios'
+Vue.prototype.$http = axios
+// 挂载到原型上
+// Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
 new Vue({
